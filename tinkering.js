56d495ae0c090f811071f951eb0d0823360bc5e4
Zach Harrison <zharrison100@gmail.com>
2020-11-09 01:22:00 +0000

    file has been updated

---
+++ tinkering.js
@@ -0,0 +1,18 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+  
+console.log(result);
+
+console.log(2 + 10 - 19 + 4 - 90 + 1);
+
+
+console.log(-20 + -19 - (-10) - (-1) + 24)
+
+
+console.log((10/5) * 4 - 20)
+
+
+console.log(4096 % 12)
