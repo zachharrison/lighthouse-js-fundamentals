a43003849faca46ea5d76ae351ee72bea72b16df
Zach Harrison <zharrison100@gmail.com>
2020-11-10 01:03:27 +0000

    more changes

---
+++ hello.js
@@ -0,0 +1,3 @@
+const sayHello = (name) => {
+  return 'Hello, ' + name;
+}
--- tinkering.js
+++ tinkering.js
@@ -3,16 +3,17 @@ function multiply(a, b) {
 }
 
 const result = multiply(2, 4);
-  
-console.log(result);
 
-console.log(2 + 10 - 19 + 4 - 90 + 1);
+// console.log(-20 + -19 - (-10) - (-1) + 24)
+// console.log((10/5) * 4 - 20)
+// console.log(4096 % 12)
 
-
-console.log(-20 + -19 - (-10) - (-1) + 24)
-
-
-console.log((10/5) * 4 - 20)
-
-
-console.log(4096 % 12)
+const whichSchool = function (age) {
+  if(age < 13){
+    return 'Elementary School';
+  } else if(age >= 13 && age <= 18){
+    return 'Secondary School';
+  } else {
+    return 'Lighthouse Labs';
+  }
+}
